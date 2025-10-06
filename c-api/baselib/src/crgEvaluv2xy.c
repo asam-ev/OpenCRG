27fc5a55da0c466b3911fecafe024ea4fccf7a23
Ahmed Sadek <164015259+asadekasam@users.noreply.github.com>
2025-10-06 12:58:57 +0200

    Update crgEvaluv2xy.c
    Ammending license statement in sample C file. To be followed with similar changes in all source files

--- c-api/baselib/src/crgEvaluv2xy.c
+++ c-api/baselib/src/crgEvaluv2xy.c
@@ -11,9 +11,20 @@
  * author:                ASAM e.V.
  * 
  * 
- * C by ASAM e.V., 2020
- * Any use is limited to the scope described in the license terms.
- * The license terms can be viewed at www.asam.net/license
+ * Original code by VIRES Simulationstechnologie GmbH, modified by ASAM e.V. 2025. See the NOTICE file 
+ * distributed with this work for additional information regarding copyright ownership.
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  * 
  * More Information on ASAM OpenCRG can be found here:
  * https://www.asam.net/standards/detail/opencrg/
